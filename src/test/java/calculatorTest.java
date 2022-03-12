8e438508b591c18c5612d19c77d31bed89008ad4
neeraj <neerajnani211@gmail.com>
2022-03-12 23:06:40 +0100

    Neeraj Failed test case 1

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -12,6 +12,16 @@ public class calculatorTest {
         assertEquals(3,a.addition(4,-1));
         assertEquals(-5,a.addition(-7,2));
     }
+    @Test
+    //test cases by Neeraj
+    public  void sub_test(){
+        calculator b=new calculator();
+
+        assertEquals(4,b.subtraction(-1,-2));
+        assertEquals(-6,b.subtraction(2,-5));
+        assertEquals(3,b.subtraction(-1,-1));
+        assertEquals(-5,b.subtraction(-7,-2));
+    }
 
 
 }
