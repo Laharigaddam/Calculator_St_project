94ece19ddc680b19946576d0546589e65cc4e52a
lahari <laharigaddam17@gmail.com>
2022-03-12 23:33:45 +0530

    Test case failed by Lahari

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -4,12 +4,13 @@ import static org.junit.Assert.*;
 
 public class calculatorTest {
     @Test
+    //test case by lahari
     public void add_test(){
         calculator a = new calculator();
-        assertEquals(4,a.addition(2,1));
-        assertEquals(-6,a.addition(-1,-4));
-        assertEquals(3,a.addition(4,-6));
-        assertEquals(-5,a.addition(-7,8));
+        assertEquals(4,a.addition(2,3));
+        assertEquals(-6,a.addition(-1,-3));
+        assertEquals(3,a.addition(4,-7));
+        assertEquals(-5,a.addition(-7,9));
     }
 
 
