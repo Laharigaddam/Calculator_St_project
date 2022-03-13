d62eba0acf73630600dec0c58bc4f8bc7300b316
Martin <mapl21@student.bth.se>
2022-03-13 02:29:48 +0100

    Multiplication and division tests with new test design

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -6,14 +6,11 @@ public class calculator {
     public int subtraction(int a, int b) {
         return a-b;
     }
-    public int multiplication(int a, int b) {
-        return a*b;
+    public String multiplication(String a, String b) {
+        return null;
     }
-    public int division(int a, int b) {
-        if(b==0){
-            throw new IllegalArgumentException("Given number can not be divided by 0!");
-        }
-        return a/b;
+    public String division(String a, String b) {
+        return null;
     }
 
     }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -3,24 +3,41 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class calculatorTest {
+
+    public final calculator calc = new calculator();
+
     @Test
     //test case by lahari
     public void add_test(){
-        calculator a = new calculator();
-        assertEquals(4,a.addition(2,2));
-        assertEquals(-6,a.addition(-1,-5));
-        assertEquals(3,a.addition(4,-1));
-        assertEquals(-5,a.addition(-7,2));
+        assertEquals(4,calc.addition(2,2));
+        assertEquals(-6,calc.addition(-1,-5));
+        assertEquals(3,calc.addition(4,-1));
+        assertEquals(-5,calc.addition(-7,2));
     }
     @Test
     //test cases by Neeraj
     public  void sub_test(){
-        calculator b=new calculator();
+        assertEquals(4,calc.subtraction(9,5));
+        assertEquals(-6,calc.subtraction(5,11));
+        assertEquals(3,calc.subtraction(7,4));
+        assertEquals(-5,calc.subtraction(2,7));
+    }
 
-        assertEquals(4,b.subtraction(9,5));
-        assertEquals(-6,b.subtraction(5,11));
-        assertEquals(3,b.subtraction(7,4));
-        assertEquals(-5,b.subtraction(2,7));
+    @Test
+    //test cases by Martin
+    public  void mul_test(){
+        assertEquals("9",calc.multiplication("3","3"));
+        assertEquals("-9",calc.multiplication("3","-3"));
+        assertEquals("1,00000E+20",calc.multiplication("9999999999","9999999999"));
+        assertEquals("-1,00000E+20",calc.multiplication("9999999999","-9999999999"));
+    }
+
+    @Test
+    public  void div_test(){
+        assertEquals("1",calc.division("3","3"));
+        assertEquals("-1",calc.division("3","-3"));
+        assertEquals("1,00000E-10",calc.division("1","9999999999"));
+        assertEquals("-1,00000E-10",calc.division("1","-9999999999"));
     }
 
 
