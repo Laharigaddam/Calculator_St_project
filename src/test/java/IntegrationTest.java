15ca2fcf73a99acaa0745993d3c48e99b5188b31
Marcin Pawlowski <mapl21@student.bth.se>
2022-03-14 20:45:26 +0100

    Changed Integration tests to using files for test sets rather than being hard coded

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,67 +1,87 @@
 import org.junit.Test;
 
+import java.util.StringTokenizer;
+
 import static org.junit.Assert.assertEquals;
 
 public class IntegrationTest {
 
     public final calculator calc = new calculator();
+    public final fileReader read = new fileReader();
 
     @Test
     public void IT_Add_Sub(){
-        assertEquals( "3", calc.addition(calc.subtraction("2" ,"1"),"2"));
-        assertEquals( "5", calc.addition(calc.subtraction("2" ,"-1"),"2"));
-        assertEquals( "-1", calc.addition(calc.subtraction("-2" ,"1"),"2"));
-        assertEquals( "-3", calc.addition(calc.subtraction("-2" ,"-1"),"-2"));
-        assertEquals( "-4", calc.addition(calc.subtraction("-2" ,"-1"),"-3"));
+        read.loadFile("ITAddSubTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.addition(calc.subtraction(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
 
     @Test
     public void IT_Sub_Add(){
-        assertEquals( "1", calc.subtraction(calc.addition("2" ,"1"),"2"));
-        assertEquals( "-1", calc.subtraction(calc.addition("2" ,"-1"),"2"));
-        assertEquals( "-3", calc.subtraction(calc.addition("-2" ,"1"),"2"));
-        assertEquals( "-1", calc.subtraction(calc.addition("-2" ,"-1"),"-2"));
+        read.loadFile("ITSubAddTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.subtraction(calc.addition(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
     @Test
     public void IT_Mul_Add(){
-        assertEquals( "6", calc.multiplication(calc.addition("2" ,"1"),"2"));
-        assertEquals( "2", calc.multiplication(calc.addition("2" ,"-1"),"2"));
-        assertEquals( "-2", calc.multiplication(calc.addition("-2" ,"1"),"2"));
-        assertEquals( "6", calc.multiplication(calc.addition("-2" ,"-1"),"-2"));
+        read.loadFile("ITMulAddTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.multiplication(calc.addition(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
 
     @Test
     public void IT_Mul_Sub(){
-        assertEquals( "2", calc.multiplication(calc.subtraction("2" ,"1"),"2"));
-        assertEquals( "6", calc.multiplication(calc.subtraction("2" ,"-1"),"2"));
-        assertEquals( "-6", calc.multiplication(calc.subtraction("-2" ,"1"),"2"));
-        assertEquals( "2", calc.multiplication(calc.subtraction("-2" ,"-1"),"-2"));
+        read.loadFile("ITMulSubTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.multiplication(calc.subtraction(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
 
     @Test
     public void IT_Mul_Div(){
-        assertEquals( "4", calc.multiplication(calc.division("2" ,"1"),"2"));
-        assertEquals( "-4", calc.multiplication(calc.division("2" ,"-1"),"2"));
-        assertEquals( "-4", calc.multiplication(calc.division("-2" ,"1"),"2"));
-        assertEquals( "-4", calc.multiplication(calc.division("-2" ,"-1"),"-2"));
+        read.loadFile("ITMulDivTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.multiplication(calc.division(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
     @Test
     public void IT_Div_Add(){
-        assertEquals( "2", calc.division(calc.addition("2" ,"2"),"2"));
-        assertEquals( "3", calc.division(calc.addition("7" ,"-1"),"2"));
-        assertEquals( "4", calc.division(calc.addition("-2" ,"10"),"2"));
-        assertEquals( "6", calc.division(calc.addition("-2" ,"-10"),"-2"));
+        read.loadFile("ITDivAddTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.division(calc.addition(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
     @Test
     public void IT_Div_Sub(){
-        assertEquals( "-1", calc.division(calc.subtraction("2" ,"4"),"2"));
-        assertEquals( "4", calc.division(calc.subtraction("2" ,"-6"),"2"));
-        assertEquals( "-7", calc.division(calc.subtraction("-13" ,"1"),"2"));
-        assertEquals( "-3", calc.division(calc.subtraction("-2" ,"-8"),"-2"));
+        read.loadFile("ITDivSubTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals(tokenizer.nextToken(), calc.division(calc.subtraction(tokenizer.nextToken(), tokenizer.nextToken()), tokenizer.nextToken()));
+        }
     }
     @Test
     public void IT_Add_Sub_Mul_Div(){
-        assertEquals( "3", calc.addition(calc.subtraction(calc.multiplication(calc.division("6","2"), "2"), "5"),"2"));
-        assertEquals( "1", calc.addition(calc.subtraction(calc.multiplication(calc.division("6","-2"), "2"), "-5"),"2"));
+        read.loadFile("ITAllBasicOpsTests");
+        String line;
+        while((line = read.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line,",");
+            assertEquals( tokenizer.nextToken(), calc.addition(calc.subtraction(calc.multiplication(calc.division(tokenizer.nextToken(),tokenizer.nextToken()), tokenizer.nextToken()), tokenizer.nextToken()),tokenizer.nextToken()));
+        }
     }
 }
